e678588d72e5b63a1fa6f1c6811a560985f85929
Seymour-Lee <894951782@qq.com>
2019-03-25 11:54:39 -0700

    add

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ const port = 8000
 // 1. use express.static to set path to static resource, like image, css, js. In this way, could use 
 //    url to get static resource(unnecessary for this proj)
 
+const ebay_key = 'YimingLi-csci571h-PRD-2a6d0a603-ae320f66'
+const google_key = 'AIzaSyAe98ZgzyxL-Y_yDJwyUxLZNIAYiOLhCkE'
+const google_engine = '017216013711347458427:6m1-yskpuj8'
+const facebook_key = '316200389071168'
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send(test()))
 
 app.get('/searchProducts', function(req, res) {
     res.sendfile('index.html');
@@ -22,9 +27,33 @@ app.get('/index.js', function(req, res) {
     res.sendfile('index.js');
 });
 
+app.get('/search', function(req, res){
+    console.log(req);
+    res.send('search return')
+});
+
 
 var server = app.listen(port, function(){
     console.log(`Example app listening on port ${port}!`);
 });
+
+function test(){
+    return 'hello world'
+}
+
+function ebay_search(){
+
+}
+
+function facebook_post(){
+
+}
+
+// https://www.googleapis.com/customsearch/v1?q=iphone&cx=017216013711347458427:6m1-yskpuj8&imgSize=huge&imgType=news&num=8&searchType=image&key=AIzaSyAe98ZgzyxL-Y_yDJwyUxLZNIAYiOLhCkE
+
+function google_image(){
+ 
+
+}
 
 // var server = app.listen(port, () => console.log(`Example app listening on port ${port}!`))
