29edf646ab1edfb52088e962c2730260dad36b1a
Seymour-Lee <894951782@qq.com>
2019-03-26 09:28:11 -0700

    add

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const https = require('https')
+const http = require('http')
 const app = express()
-const port = 8000
+const port = 8081
 
 // server side, I use express.js.
 // server only gets ajax from client, call api, parse result, return data which is needed for client
@@ -12,37 +14,160 @@ const google_key = 'AIzaSyAe98ZgzyxL-Y_yDJwyUxLZNIAYiOLhCkE'
 const google_engine = '017216013711347458427:6m1-yskpuj8'
 const facebook_key = '316200389071168'
 
-// app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/', (req, res) => res.send(test()))
+var category2id = {
+    "art": "550",
+    "baby": "2984",
+    "books": "267",
+    "clothings": "11450",
+    "computers": "58058",
+    "health": "26395",
+    "music": "11233",
+    "video": "1249",
+};
 
-app.get('/searchProducts', function(req, res) {
-    res.sendfile('index.html');
+const ebay_keyword_url = 'http://svcs.ebay.com/services/search/FindingService/v1?'
+
+app.use(express.static(__dirname + '/node_modules'));
+
+app.get('/', (req, res) => res.send(test()));
+app.get('/index.css', (req, res) => res.sendfile('index.css'));
+app.get('/index.js', (req, res) => res.sendfile('index.js'));
+app.get('/searchProducts', (req, res) => res.sendfile('index.html'));
+app.get('/search_keyword', (req, res) => ebay_search_keyword(req, res));
+
+app.get('/search_item', function(req, res){
+    console.log(req.query);
+    // call ebay search item api
+    res.send(req.query)
 });
-
-app.get('/index.css', function(req, res) {
-    res.sendfile('index.css');
-});
-
-app.get('/index.js', function(req, res) {
-    res.sendfile('index.js');
-});
-
-app.get('/search', function(req, res){
-    console.log(req);
-    res.send('search return')
-});
-
 
 var server = app.listen(port, function(){
-    console.log(`Example app listening on port ${port}!`);
+    console.log(`express.js app listening on port ${port}!`);
 });
 
 function test(){
     return 'hello world'
 }
 
-function ebay_search(){
+function ebay_search_keyword(req, res){
+    // console.log(req.query);
+    // call ebay search keyword api
+    var param = req.query;
+    var url = ebay_keyword_url;
+    // {   categpry: 'computers',
+    //     distance: 'aaa',
+    //     free: 'false',
+    //     from: 'location', // no need for this in ebay api
+    //     keyword: 'dasdasd',
+    //     local: 'true',
+    //     new: 'false',
+    //     unspecified: 'true',
+    //     used: 'false',
+    //     zipcode: '' }
 
+    // http://svcs.ebay.com/services/search/FindingService/v1?
+    // OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0
+    // &SECURITY-APPNAME=[APP- ID]&RESPONSE-DATA-FORMAT=JSON&REST- PAYLOAD
+    // &paginationInput.entriesPerPage=50&keywords=iphone&buyerPostalCode=90007
+    // &itemFilter(0).name=MaxDistance&itemFilter(0).value=10
+    // &itemFilter(1).name=FreeShipping Only&itemFilter(1).value=true
+    // &itemFilter(2).name=LocalPickupOnly&itemFilter(2).value=true
+    // &itemFilter(3).name=HideDuplicateItems&itemFilter(3).value=true
+    // &itemFilter(4).name=Condition&itemFilter(4).value(0)=New
+    // &itemFilter(4).value(1)=Used&itemFilter(4).value(2)=Unspecified
+    // &outputSelector(0)=SellerInfo&outputSelector(1)=StoreInfo
+
+    url += 'OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0';
+    url += '&SECURITY-APPNAME=' + ebay_key;
+    url += '&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&paginationInput.entriesPerPage=50';
+    url += '&keywords=' + param.keyword;
+    url += '&buyerPostalCode=' + param.zipcode;
+    if(param.categpry != 'all'){
+        url += '&categoryId=' + category2id[param.categpry];
+    }
+    url += '&itemFilter(0).name=MaxDistance&itemFilter(0).value=' + param.distance;
+    url += '&itemFilter(1).name=FreeShipping Only&itemFilter(1).value=' + param.free;
+    url += '&itemFilter(2).name=LocalPickupOnly&itemFilter(2).value=' + param.local;
+    url += '&itemFilter(3).name=HideDuplicateItems&itemFilter(3).value=true';
+    var counter = 0;
+    if(param.new == 'true') counter += 1;
+    if(param.used == 'true') counter += 1;
+    if(param.unspecified == 'true') counter += 1;
+    if(counter > 0){
+        var i = 0;
+        url += '&itemFilter(4).name=Condition';
+        if(param.new == 'true'){
+            url += '&itemFilter(4).value('+i.toString()+')=New';
+            i += 1;
+        }
+        if(param.used == 'true'){
+            url += '&itemFilter(4).value('+i.toString()+')=Used';
+            i += 1;
+        } 
+        if(param.unspecified == 'true'){
+            url += '&itemFilter(4).value('+i.toString()+')=Unspecified';
+            i += 1;
+        } 
+    }
+    url += '&outputSelector(0)=SellerInfo&outputSelector(1)=StoreInfo';
+
+    http.get(url, (resp) => {
+        const { statusCode } = resp;
+        const contentType = resp.headers['content-type'];
+        // console.log(contentType)
+
+        let error;
+        if (statusCode !== 200) {
+            error = new Error('Request Failed.\n' +
+                            `Status Code: ${statusCode}`);
+        }
+        // text/plain;charset=UTF-8
+        else if (!/^text\/plain/.test(contentType)) {
+            error = new Error('Invalid content-type.\n' +
+                            `Expected text/plain but received ${contentType}`);
+        }
+        if (error) {
+            console.error(error.message);
+            // Consume response data to free up memory
+            resp.resume();
+            return;
+        }
+
+        // resp.setEncoding('utf8');
+        let rawData = '';
+        resp.on('data', (chunk) => { rawData += chunk; });
+        resp.on('end', () => {
+            try {
+                const parsedData = JSON.parse(rawData);
+                var items = parsedData.findItemsAdvancedResponse[0].searchResult[0].item;
+                var cols = [];
+                // handle no records
+                for(var i = 0; i < items.length; i++){
+                    var col = {};
+                    var item = items[i];
+                    col["index"] = (i+1).toString();
+                    if(item.hasOwnProperty('galleryURL')) col["image"] = item.galleryURL[0];
+                    else col["image"] = "";
+                    col["title"] = item.title;
+                    col["price"] = item.sellingStatus[0].currentPrice[0].__value__;
+                    if(item.hasOwnProperty('shippingInfo')) col["shipping"] = 'N/A';
+                    else if(item.shippingInfo[0].shippingServiceCost[0].__value__ == '0') col["shipping"] = 'Free Shipping';
+                    else col["shipping"] = item.shippingInfo[0].shippingServiceCost[0].__value__;
+                    col["zip"] = item.postalCode;
+                    col["seller"] = item.sellerInfo[0].sellerUserName[0];
+                    cols.push(col);
+                }
+                console.log(cols[49])
+                // console.log(parsedData);
+                res.send(cols)
+            } catch (e) {
+                console.error(e.message);
+            }
+        });
+    }).on('error', (e) => {
+        console.error(`Got error: ${e.message}`);
+    });
+    // res.send(req.query)
 }
 
 function facebook_post(){
--- index.js
+++ index.js
@@ -1,6 +1,61 @@
 var app = angular.module('app', [])
 
-app.controller('ctrlmaster', function($scope) {
+app.controller('ctrlmaster', function($scope, $location, $http) {
+    $scope.init = function(){
+        console.log('in init')
+        $scope.form_keyword = '';
+        $scope.form_category = 'all';
+        $scope.form_condition_new = false;
+        $scope.form_condition_used = false;
+        $scope.form_condition_unspecified = false;
+        $scope.form_shipping_local = false;
+        $scope.form_shipping_free = false;
+        $scope.form_distance = '';
+        $scope.form_from = 'location';
+        $scope.form_zipcode = '';
+    }
+
+    $scope.url = location.host;
+    $scope.items_list = [];
+
     
+    $scope.search_keyword = function() {
+        // console.log(this.form_keyword, this.form_category)
+        // console.log($scope.form_condition_new, $scope.form_condition_used, $scope.form_condition_unspecified)
+        // console.log($scope.form_shipping_local, $scope.form_shipping_free, $scope.form_distance)
+        // console.log($scope.form_from, $scope.form_zipcode)
+        // ajax to express.js
+        $http({
+            url: 'http://' + $scope.url + '/search_keyword',
+            method: "GET",
+            params:{
+                keyword: $scope.form_keyword,
+                categpry: $scope.form_category,
+                new: $scope.form_condition_new,
+                used: $scope.form_condition_used,
+                unspecified: $scope.form_condition_unspecified,
+                local: $scope.form_shipping_local,
+                free: $scope.form_shipping_free,
+                distance: $scope.form_distance,
+                from: $scope.form_from,
+                zipcode: $scope.form_zipcode
+            }
+        }).then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+            $scope.items_list = response.data;
+            console.log($scope.items_list)
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+
+    };
+
+    $scope.search_item = function(){
+
+    };
+
+
 });
 
